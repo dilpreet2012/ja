e8c22b17b485801626b644d80fb6362e9746d8fc
dilpreet2012 <dilkill2000@gmail.com>
2024-04-29 15:57:18 +0530

    typeconversion

---
+++ 04basics.js
@@ -0,0 +1,24 @@
+//type conversion 
+// let a="23"
+// console.log(a,typeof a);
+// let b=Number(a);
+// console.log(b,typeof b);
+// let a="Khushi";
+// console.log(a,typeof a);
+// let b=Number(a);
+// console.log(b,typeof b);
+// let  a=false;
+// console.log(a,typeof a);
+// let b=Number(a);
+// console.log(b,typeof b);
+// let  a=undefined;
+// console.log(a,typeof a);
+// let b=Number(a);
+// console.log(b,typeof b);
+// let  a=null;
+// console.log(a,typeof a);
+// let b=Number(a);
+// console.log(b,typeof b);
+let a=-20;
+let b=Boolean(a);
+console.log(b,typeof b);
