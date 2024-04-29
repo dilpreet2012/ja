0da051645e983b38fd4cab343bd3f9e82c441b47
dilpreet2012 <dilkill2000@gmail.com>
2024-04-29 21:38:41 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,14 @@
+let a="Khushi"
+let age=23;
+const energy="main";
+console.log(`hello, i am ${a} and i am ${age} years old and i have energy of ${energy} character`);
+//console.log(a,typeof a);
+// let b=new String("khushi");
+// console.log(b,typeof b);
+// console.log(b.toUpperCase());
+// console.log(a.toUpperCase());
+// console.log(a.substring(1,4));
+// console.log(a.slice(-6,-2));
+// console.log(a.toLowerCase());
+// console.log(a.split('u'));
+// console.log(a.trim());
