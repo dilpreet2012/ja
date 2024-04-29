493807fea1bdfd0dd200755b3c0ca445bb25dc57
dilpreet2012 <dilkill2000@gmail.com>
2024-04-29 17:03:33 +0530

    operators in js

---
+++ 05basics.js
@@ -0,0 +1,21 @@
+//operators in javascript
+//arithmetic operator
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+// console.log(2+"4");
+// console.log("2"+4);
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+// console.log(1+"2"+2);
+console.log("hello "+" world")
+
+
+//comparsion operator
+console.log(2===2)
+console.log(4>2)
+console.log(2==="2");
+console.log(5&&4);
