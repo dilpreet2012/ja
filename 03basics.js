439f6f9f9ea88eec741fc0a1e6cc6f1ca51504b4
dilpreet2012 <dilkill2000@gmail.com>
2024-04-29 15:12:24 +0530

    data types

---
+++ 03basics.js
@@ -0,0 +1,32 @@
+//data typesi in javascript-(Primitive)
+"use strict;"// treat all JS code as newer version
+// let a=34;
+// let b=45.67;
+// let c="khushi";
+// let d='muskaan'
+// console.table([a,b,c,d,typeof a,typeof b,typeof c,typeof d]);
+// let a;
+// let b=undefined;
+// let c=true;
+// let d=null;
+// console.table([a,b,c,d,typeof a,typeof b,typeof c,typeof d]);
+// let a=Symbol('khushi');
+// console.log(a,typeof a);
+// let b=Symbol('khushi');
+// console.log(b,typeof b);
+// console.log(a==b);
+// console.log(a===b);
+// let c="khushi";
+// let d="khushi";
+// console.log(c==d);
+// console.log(c===d);
+
+let a={name:"dilpreet",age:23,city:"sultanpur"}
+console.log(a,typeof a);
+let b=[1,4.5,"khushi",false,undefined];
+console.log(b,typeof b);
+let c=()=>{
+    console.log("i am a function");
+}
+console.log(c);
+console.log(typeof c);
