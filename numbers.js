aeec64801b67ed64c6ee8f4a8ea89a17ee9d214b
dilpreet2012 <dilkill2000@gmail.com>
2024-05-09 18:19:11 +0530

    numbers

---
+++ numbers.js
@@ -0,0 +1,17 @@
+// const num=45;
+// console.log(num,typeof num);
+// const num2=new Number(12);
+// console.log(num2,typeof num2);
+// console.log(num2.toString().length);
+// console.log(Number.MIN_VALUE);
+//  ++++++++++++++Maths++++++++++++++++++++++
+// console.log(Math.abs(-9));
+// console.log(Math.round(7.2));
+// console.log(Math.floor(5.934));
+// console.log(Math.ceil(5.0092));
+// console.log(Math.PI);
+// console.log(Math.pow(4,3));
+console.log(Math.random());//generates random number between 0 and 1
+console.log(Math.floor(Math.random()*10)+1);//between 1 to 10
+//if we want to generate numbers between 20 to 30
+console.log(Math.floor(Math.random()*10)+20)
